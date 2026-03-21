474b83bf9cc954d304d21ad4435146a84d4c8d20
sungjinnn <127401478+sungjinnn@users.noreply.github.com>
2026-03-21 22:15:54 +0900

    Update and rename 2장 과제2.cpp to 2장 과제2.1.cpp

---
+++ 과제/2장/2장 과제2.1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h> //strlen 함수를 위해 필요
+#define _CRT_SECURE_NO_WARNINGS 
+
+int main() {
+	char text[100];
+	int length;
+
+	printf("문자열을 입력하세요 : ");
+	scanf_s("%s", text,100);
+	length = strlen(text);
+
+	printf("입력된 문자열은\n\"%s!\"\n입니다.\n", text);
+
+	printf("입력된 문자열의 길이 = %d\n", length);
+
+	return 0;
+}
